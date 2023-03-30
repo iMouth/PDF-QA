23fa95c230479e979e2904f42388ba336bade4c4
Kelvin Etori <kelvin.8.kast@gmail.com>
2023-03-30 03:02:11 -0700

    converts test pdfs to json with scipy parser and s2orc parser

--- parser/run.sh
+++
@@ -1,6 +0,0 @@
-pip install git+https://github.com/titipata/scipdf_parser
-pip3 install -U spacy
-python3 -m spacy download en_core_web_sm
-pip3 install bs4
-# Must be on Java 8 for Grobid
-bash serve_grobid.sh
--- parser/run2.sh
+++
@@ -1,9 +0,0 @@
-# You need miniconda https://docs.conda.io/en/latest/miniconda.html
-
-git clone git@github.com:allenai/s2orc-doc2json.git
-cd s2orc-doc2json
-pip3 install -r requirements.txt
-python3 setup.py develop
-bash scripts/setup_grobid.sh
-bash scripts/run_grobid.sh
-cd ..
