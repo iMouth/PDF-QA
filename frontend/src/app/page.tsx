b56088c654a3d420a2d41454daf70ac840ba39c5
Kelvin Etori <kelvin.8.kast@gmail.com>
2023-04-07 22:53:51 -0700

    part of frontend created

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./landing.css"
+
+const Landing = () => {
+  return (
+    <div id='Landing'>
+      		<div className="box" id="message-box"></div>
+          <div className="inputs">
+            <input type="text" className="question" placeholder="Enter a question..."></input>
+            <input type="file" className="file-picker" accept=".pdf"></input>
+          </div>
+    </div>
+  )
+}
+
+export default Landing
