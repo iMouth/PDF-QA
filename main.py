ca9f625134ce0ca0fd4462663ab644d58aa690d0
sadafrf <sadafraoufi98@gmail.com>
2023-04-19 09:58:00 -0700

    added new fields to schema

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class SearchEngine:
     def __init__(self, index_dir):
         self.index_dir = index_dir
         self.schema = Schema(
-            id=ID(stored=True), title=TEXT(stored=True), content=TEXT(stored=True), score=NUMERIC(stored=True)
+            id=ID(stored=True), title=TEXT(stored=True), abstract=TEXT(stored=True), score=NUMERIC(stored=True),question=TEXT(stored=True),answer=TEXT(stored=True),section=TEXT(stored=True)
         )
         self.index = create_in(self.index_dir, self.schema)
         self.searcher = self.index.searcher()
@@ -20,13 +20,18 @@ class SearchEngine:
         for file_path in json_files:
             with open(file_path, "r", encoding="utf-8") as f:
                 json_data = json.load(f)
-                writer.add_document(
-                    id=str(json_data["paper_id"]), title=json_data["title"], content=json_data["abstract"]
-                )
+
+                # writer.add_document(id=str(json_data["paper_id"]), title=json_data["title"], abstract=json_data["abstract"],section=json_data["section"])
+                # writer.commit()
+                
+                for i in range(len(json_data["question_answer_pairs"])):
+                    writer.add_document(question=json_data["question"][i],answer=json_data["answers"][i])
+
+                
         writer.commit()
 
     def search(self, query_string):
-        parser = QueryParser("content", self.schema)
+        parser = QueryParser("answers", self.schema)
         query = parser.parse(query_string)
         tfidf_scoring = scoring.TF_IDF()
         with self.index.searcher() as searcher:
@@ -46,10 +51,7 @@ if __name__ == "__main__":
         "parser/formatted_s2orc/JSON_files/A study of wood burning and traffic aerosols in an Alpine valley using a multi-wavelength Aethalometer.json"
     ]
     search_engine.index_files(json_files)
-    # print index
-    # search_engine.print_index()
-
-    # search
-    ids = search_engine.search("present")
+    
+    ids = search_engine.search("What emissions are produced by burning wood?")
 
     print(ids)
